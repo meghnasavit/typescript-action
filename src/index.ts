a4ca879eb37cb8ba8576143f9ae4f6d6b0eb5b9f
meghnasavit <meghna.savit99@gmail.com>
2020-06-14 22:54:09 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 
 function run() {
   const name: string = core.getInput('my_input');
+  console.log("fsdfdsfdsfds");
   if (name) {
     core.debug(`Hello ${name}!`);
     return core.setOutput('my_output', `Hello ${name}!`);

59b7578a293e365f177a60fccad31cb6d2ba7b6f
meghnasavit <meghna.savit99@gmail.com>
2020-06-15 00:43:33 +0530

    repository dispatch

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as github from '@actions/github';
 function run() {
   const name: string = core.getInput('my_input');
   console.log("fsdfdsfdsfddddds");
-  const octokit = new Octokit();
+  const octokit = github.getOctokit('bb67194b142e4234b2f165d08b156dce1247db95')
   const { owner, repo } = github.context.repo;
   const event_type = 'custom';
   octokit.repos.createDispatchEvent({
